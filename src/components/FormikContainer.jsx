ec0d75141e4f116e0469ec8200a1474f0684ab1d
Edwin Roman <pazamoryperdon@gmail.com>
2021-01-17 20:10:56 -0500

    "Input control"

--- src/components/FormikContainer.jsx
+++ src/components/FormikContainer.jsx
@@ -1,17 +1,33 @@
 import React from 'react'
 import { Formik, Form } from 'formik'
 import * as Yup from 'yup'
+import FormikControl from './FormikControl'
 
 function FormikContainer() {
-    const initialValues = {}
-    const validationSchema = Yup.object({})
+    const initialValues = {
+        email:''
+    }
+    const validationSchema = Yup.object({
+        email: Yup.string().required("Correo es requerido").email()
+    })
     const onSubmit = values => console.log('Form data', values)
     return (
-        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
-            {
-                formik = <Form>
-                    <submit type='submit'>Enviar</submit>
+        <Formik 
+            initialValues={initialValues} 
+            validationSchema={validationSchema} 
+            onSubmit={onSubmit}
+        >
+            {formik => (
+                <Form>
+                    <FormikControl 
+                        control='input' 
+                        type='email' 
+                        label='Email' 
+                        name='email' 
+                    />
+                    <button type='submit'>Enviar</button>
                 </Form>
+                )
             }
         </Formik>
     )
